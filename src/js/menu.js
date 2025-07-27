d1a82844ff6027555ebcc5d566ab145835f1c6c7
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-07-28 00:28:40 +0300

    Fixed bugs , add javascript, add transition for buttons

---
+++ src/js/menu.js
@@ -0,0 +1,77 @@
+// Header
+
+const openMenuBtn = document.querySelector('.mobile-menu-open-btn');
+const closeMenuBtn = document.querySelector('.pop-up-close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+});
+
+// Кнопка join now
+
+document.querySelector('.header-button').addEventListener('click', () => {
+  document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
+});
+
+// Hero-section
+
+document.querySelector('.button-pricing').addEventListener('click', () => {
+  document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.button-gallery').addEventListener('click', () => {
+  document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
+});
+
+// Yoga-classes
+document.querySelector('.yoga-button').addEventListener('click', () => {
+  document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
+});
+
+// About-us
+document.querySelector('.about-us-join-btn').addEventListener('click', () => {
+  document.querySelector('#pricing').scrollIntoView({ behavior: 'smooth' });
+});
+
+// Modal
+document.querySelectorAll('[data-modal-close]').forEach(button => {
+  button.addEventListener('click', () => {
+    document.querySelectorAll('.backdrop.is-open').forEach(modal => {
+      modal.classList.remove('is-open');
+    });
+  });
+});
+//  10$ modal window
+
+const openModal10 = document.querySelector('.pricing-button-10');
+const modal10 = document.querySelector('.modal-10');
+
+openModal10.addEventListener('click', () => {
+  modal10.classList.add('is-open');
+});
+
+// 65$ modal window
+const openModal65 = document.querySelector('.pricing-button-65');
+const modal65 = document.querySelector('.modal-65');
+
+openModal65.addEventListener('click', () => {
+  modal65.classList.add('is-open');
+});
+
+// 80$ window
+document.querySelectorAll('.pricing-button-80').forEach(button => {
+  button.addEventListener('click', () => {
+    modal80.classList.add('is-open');
+  });
+});
+
+const modal80 = document.querySelector('.modal-80');
+
+openModal80.addEventListener('click', () => {
+  modal80.classList.add('is-open');
+});
